0aaadd009a870662d8ad7c35e23cd516335ba662
mathijs159 <mathijs159@users.noreply.github.com>
2015-03-23 13:59:12 +0100

    Test 2

--- annotationViewer/src/annotationviewer/AnnotationViewer.java
+++ annotationViewer/src/annotationviewer/AnnotationViewer.java
@@ -15,7 +15,7 @@ public class AnnotationViewer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("print");
+        System.out.println("test2");
     }
     
 }
