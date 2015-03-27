bfe11071491a25224c4c205c38544baa6dccf36f
mathijs159 <mathijs159@users.noreply.github.com>
2015-03-27 20:07:19 +0100

    Expanded DB connect, still does not connect

--- annotationViewer/src/annotationviewer/dbConnect.java
+++ annotationViewer/src/annotationviewer/dbConnect.java
@@ -15,17 +15,40 @@ import java.util.logging.Logger;
  */
 public class dbConnect {
 
-        static String MySQL_Driver = "jdbc:mysql://";
-        static String userForm = "?user=";
+    static String MySQL_Driver = "jdbc:mysql://";
+    static String userForm = "?user=";
+    static Statement stmt = null;
+    static ResultSet rs = null;
 
     public static void dbConnect(String query, String url_port, String user) {
+
         try {
-            Connection conn = 
-                    DriverManager.getConnection(MySQL_Driver+url_port+userForm+user);
+            Connection conn
+                    = DriverManager.getConnection(MySQL_Driver + url_port + userForm + user);
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+            //return rs;
+
         } catch (SQLException ex) {
             Logger.getLogger(dbConnect.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+
+                } catch (SQLException sqlEx) {
+                }
+                rs = null;
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException sqlEx) {
+            }
+            stmt = null;
         }
 
     }
-
 }
